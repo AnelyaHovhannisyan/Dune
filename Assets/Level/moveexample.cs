b5767e6b09a151c79db7df581a3075663d359705
Anelya <anelyahovhannisyan1999@gmail.com>
2021-08-10 17:13:03 +0400

    no message

---
+++ Assets/Level/moveexample.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveexample : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject character;
+
+    [SerializeField]
+    private Rigidbody characterBody;
+    //private float ScreenWidth;
+    [SerializeField]
+    private float fallSpeed;
+
+
+    [SerializeField] TouchManager touchManager;
+
+
+
+    private void Awake()
+    {
+        touchManager.OnTouchDown += OnTouchDown;
+        touchManager.OnTouchUp += OnTouchUp;
+        touchManager.OnTouchDrag += OnTouchDrag;
+    }
+
+    void OnTouchDown(Vector2 touch)
+    {
+        if (PullBallRoutineC != null) StopCoroutine(PullBallRoutineC);
+        PullBallRoutineC = StartCoroutine(PullBallRoutine());
+    }
+
+
+    void OnTouchUp(Vector2 touch)
+    {
+        if (PullBallRoutineC != null) StopCoroutine(PullBallRoutineC);
+    }
+
+
+    void OnTouchDrag(Vector2 touch, Vector2 deltaTouch)
+    {
+
+    }
+
+
+    Coroutine PullBallRoutineC;
+    IEnumerator PullBallRoutine()
+    {
+        while(true)
+        {
+            characterBody.AddForce(Vector3.down * fallSpeed, ForceMode.Force);
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+
+
+    //void Start()
+    //{
+    //    ScreenWidth = Screen.width;
+     
+    //}
+
+    
+ 
+
+  
+}
---
+++ Assets/Level/scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float speed = 10.0f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+
+        float sideForce = Input.GetAxis("Horizontal") * speed;
+       
+        rb.AddForce(0f, 0f, sideForce);
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+}
+
